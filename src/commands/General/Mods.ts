2e8f7445120fbb1afbb6c13e23237749731faefa
well300 <90952986+well300@users.noreply.github.com>
2021-09-18 13:56:50 +0530

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -24,7 +24,7 @@ export default class Command extends BaseCommand {
                     user.notify || user.vname || user.name || 'null'
                 }*\n🍀 *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
         )
-        text += `\nTo deploy your own Bot or To support Star ✨\nVisit : https://github.com/Nakshatra4762/Star-2.0`
+        text += `\nTo deploy your own Bot or To support Message wa.me/917993034080`
         return void M.reply(text)
     }
 }
