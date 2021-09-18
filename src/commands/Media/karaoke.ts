a522215de0750e7d81925d501423bc86209ad371
well300 <90952986+well300@users.noreply.github.com>
2021-09-18 14:14:14 +0530

    Update karaoke.ts

--- src/commands/Media/karaoke.ts
+++ src/commands/Media/karaoke.ts
@@ -23,7 +23,7 @@ export default class Command extends BaseCommand {
         const term = joined.trim()
         const { videos } = await yts(term + ' karaoke song')
         if (!videos || videos.length <= 0) return void M.reply(`No Matching videos found for the term *${term}*`)
-        let text = `Via ✨Star✨`
+        let text = `Via 🖤Eren🖤`
 
         this.client
             .sendMessage(M.from, text, MessageType.extendedText, {
@@ -31,7 +31,7 @@ export default class Command extends BaseCommand {
                 contextInfo: {
                     externalAdReply: {
                         title: `Search Term: ${term}`,
-                        body: `✨Made for you✨`,
+                        body: `🖤Made for you sowm🖤`,
                         mediaType: 2,
                         thumbnailUrl: videos[0].thumbnail,
                         mediaUrl: videos[0].url
