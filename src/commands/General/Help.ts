482fe42424e6a242fcc0d2798bc1d8c3b6f38052
well300 <90952986+well300@users.noreply.github.com>
2021-09-18 13:17:54 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -32,7 +32,7 @@ export default class Command extends BaseCommand {
                     categories[info.config.category].push(info)
                 }
             }
-            let text = `🖤 *Eren's Command List* 🖤\n\n`
+            let text = `🖤 *𝐄𝐫𝐞𝐧'𝐬 𝐂𝐨𝐦𝐦𝐚𝐧𝐝 𝐋𝐢𝐬𝐭* 🖤\n\n`
             const keys = Object.keys(categories)
             for (const key of keys)
                 text += `${this.emojis[keys.indexOf(key)]} *${this.client.util.capitalize(key)}*\n❐ \`\`\`${categories[
