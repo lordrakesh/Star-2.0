f8afd5b6e1947773680c29d653d6d765040b3370
well300 <90952986+well300@users.noreply.github.com>
2021-09-18 14:20:22 +0530

    Update neko.ts

--- src/commands/Weeb/neko.ts
+++ src/commands/Weeb/neko.ts
@@ -33,7 +33,7 @@ export default class Command extends BaseCommand {
                     MessageType.image,
                     undefined,
                     undefined,
-                    `Here you go ✨\n`,
+                    `Here you go 🖤\n`,
                     undefined
                 )
                 break
